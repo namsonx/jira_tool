f00cc86104f0f442fde601a47b044e7d79fee93b
Linh-PC <Linh-PC@Linh-Tran>
2016-12-09 23:26:28 +0700

    updated

--- jira_tool/jira_tool.py
+++ jira_tool/jira_tool.py
@@ -10,6 +10,7 @@ def main():
     username = raw_input('Enter your username: ')
     password = raw_input('Enter your password: ')
     connected = con_jira(username, password)
+    raw_input('Press Enter to exit')
 
 if __name__ == '__main__':
     main()
