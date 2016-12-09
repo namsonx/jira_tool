4811524ac84520d675977cf6a62911d503c289a1
Linh-PC <Linh-PC@Linh-Tran>
2016-12-09 23:15:22 +0700

    first commit

---
+++ jira_tool/connected_jira.py
@@ -0,0 +1,23 @@
+'''
+Created on Dec 9, 2016
+
+@author: Linh-PC
+'''
+
+from jira import JIRA
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, username, password):
+        '''
+        Constructor
+        '''
+        self.username = username
+        self.password = password
+        print 'Username is: ', self.username
+        print 'Password is: ', self.password
+        
